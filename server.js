43f7fa0eb32dad6a32f1f719a11830b8ba78ac13
manoj-gif-b <bipothumeganadham@gmail.com>
2026-06-09 14:43:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*', methods: ['GET','POST'] } });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 const rooms = {};
 const ODDS = [1,3,5,7,9];
